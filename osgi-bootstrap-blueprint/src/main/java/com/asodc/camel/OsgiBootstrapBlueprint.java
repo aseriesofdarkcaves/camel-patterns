059a97e61bad2778815df6a4a81f6c35412e1181
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2019-12-13 13:31:59 +0100

    added new module: osgi-bootstrap-blueprint

---
+++ osgi-bootstrap-blueprint/src/main/java/com/asodc/camel/OsgiBootstrapBlueprint.java
@@ -0,0 +1,16 @@
+package com.asodc.camel;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+
+public class OsgiBootstrapBlueprint extends RouteBuilder {
+    private static final String LOGGER = OsgiBootstrapBlueprint.class.getCanonicalName();
+    private static final String LOG_MESSAGE = "HEADERS:\r\n${headers}\r\nBODY:\r\n${body}";
+    private static final String ROUTE_ID = OsgiBootstrapBlueprint.class.getCanonicalName();
+    @Override
+    public void configure() throws Exception {
+        from("timer:timer?period=5000")
+                .id(ROUTE_ID)
+                .log(LoggingLevel.INFO, LOGGER, LOG_MESSAGE);
+    }
+}
