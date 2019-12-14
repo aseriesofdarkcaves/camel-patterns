2acd3cbc2094b67303fb3390a9e926027c5101d3
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2019-12-14 16:39:35 +0100

    added content enricher EIP examples

---
+++ content-enricher/src/main/java/com/asodc/camel/PollEnrichFile.java
@@ -0,0 +1,33 @@
+package com.asodc.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.main.Main;
+
+public class PollEnrichFile {
+    public static void main(String... args) throws Exception{
+        Main main = new Main();
+        CamelContext context = new DefaultCamelContext();
+        context.setUseMDCLogging(true);
+
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() {
+                from("timer:start?delay=1000")
+                        .pollEnrich()
+                        .simple("file:data/content-enricher?fileName=Dummy.txt" +
+                                "&charset=UTF-8" +
+                                "&preMove=Staging" +
+                                "&move=Success/${file:name.noext}-${date:now:yyyyMMddHHmmssSSS}.${file:ext}" +
+                                "&moveFailed=Failed/${file:name.noext}-${date:now:yyyyMMddHHmmssSSS}.${file:ext}"
+                        )
+                        .log("Finished polling...");
+            }
+        });
+
+        context.start();
+        main.getCamelContexts().add(context);
+        main.run();
+    }
+}
